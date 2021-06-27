4cb0885ce370cc294d0f1114aaad15c8efc21d67
qux-bbb <1147635419@qq.com>
2021-06-27 17:00:47 +0800

    md5 test

--- xhash.cpp
+++ xhash.cpp
@@ -2,7 +2,7 @@
 #include <QMessageBox>
 #include <QFileDialog>
 #include <QTextStream>
-#include <QThread>
+#include <QCryptographicHash>
 #include "xhash.h"
 #include "ui_xhash.h"
 
@@ -38,14 +38,40 @@ void XHash::on_pushButton_copy_clicked()
 void XHash::on_pushButton_browse_clicked()
 {
     // TODO
-    QStringList fileNames = QFileDialog::getOpenFileNames(this, "Open file...", "", "All files (*)");
+    QStringList filePaths = QFileDialog::getOpenFileNames(this, "Open file...", "", "All files (*)");
 
-    foreach (QString fileName, fileNames) {
-        ui->textBrowser->append(fileName);
-        ui->progressBar_file->setValue(100);
-        QThread::sleep(2);
-        ui->progressBar_file->reset();
-        QThread::sleep(2);
+    int filePaths_count = filePaths.count();
+
+    if (filePaths_count==0)
+        return;
+
+    ui->progressBar_total->setValue(0);
+    ui->progressBar_total->setMaximum(filePaths_count);
+
+    foreach (QString filePath, filePaths) {
+        ui->progressBar_file->setValue(0);
+
+        QFile file(filePath);
+
+        if (!file.open(QIODevice::ReadOnly)) {
+            QMessageBox::information(this, tr("Unable to open file"),
+                                     file.errorString());
+        }else{
+            QCryptographicHash crypto(QCryptographicHash::Md5);
+            while(!file.atEnd()){
+              crypto.addData(file.read(8192));
+            }
+            QByteArray source;
+            QTextStream in(&file);
+            in >> source;
+            QString md5Value = QString(crypto.result().toHex());
+
+            ui->textBrowser->append(filePath);
+            ui->textBrowser->append("md5_value: "+md5Value+"\n");
+            ui->progressBar_file->setValue(100);
+        }
+
+        ui->progressBar_total->setValue(ui->progressBar_total->value()+1);
     }
 }
 
@@ -56,11 +82,11 @@ void XHash::on_pushButton_save_clicked()
     if (source == ""){
         QMessageBox::warning(this, "Warning", "The Content can not be empty!", QMessageBox::Ok);
     }else{
-        QString fileName = QFileDialog::getSaveFileName(this, tr("Save Result"), "", tr("Text (*.txt);;All Files (*)"));
-        if (fileName.isEmpty())
+        QString filePath = QFileDialog::getSaveFileName(this, tr("Save Result"), "", tr("Text (*.txt);;All Files (*)"));
+        if (filePath.isEmpty())
             return;
         else {
-            QFile file(fileName);
+            QFile file(filePath);
             if (!file.open(QIODevice::WriteOnly)) {
                 QMessageBox::information(this, tr("Unable to open file"),
                                          file.errorString());
@@ -78,4 +104,3 @@ void XHash::on_pushButton_stop_clicked()
     // TODO
     ui->textBrowser->setText("on_pushButton_stop_clicked");
 }
-
