5be03d3aad8645734c9d092a458a4c2da008ea48
qux-bbb <1147635419@qq.com>
2023-06-27 08:04:04 +0800

    update chinese translate

--- src/XHash_zh_CN.ts
+++ src/XHash_zh_CN.ts
@@ -2,76 +2,114 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>Config</name>
+    <message>
+        <location filename="config.ui" line="35"/>
+        <source>Language</source>
+        <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="14"/>
+        <source>Config</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="61"/>
+        <source>Add to context menu</source>
+        <translation>添加到上下文菜单</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="68"/>
+        <source>Start calculating as soon as getting path</source>
+        <translation>获取路径后自动开始计算</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="75"/>
+        <source>Default hash types</source>
+        <translation>默认的hash类型</translation>
+    </message>
+    <message>
+        <location filename="config.ui" line="144"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+</context>
+<context>
     <name>XHash</name>
     <message>
-        <location filename="xhash.ui" line="59"/>
+        <location filename="xhash.ui" line="57"/>
         <source>Drag and drop file or click &quot;Browse...&quot; button</source>
         <translation>拖放文件或点击&quot;浏览...&quot;按钮</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="68"/>
+        <location filename="xhash.ui" line="72"/>
         <source>Browse...</source>
         <translation>浏览...</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="140"/>
+        <location filename="xhash.ui" line="164"/>
         <source>Clear</source>
         <translation>清除</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="147"/>
+        <location filename="xhash.ui" line="177"/>
         <source>Copy</source>
         <translation>复制</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="154"/>
+        <location filename="xhash.ui" line="190"/>
         <source>Save...</source>
         <translation>保存...</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="82"/>
+        <location filename="xhash.ui" line="98"/>
         <source>Stop</source>
         <translation>停止</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="50"/>
+        <location filename="xhash.ui" line="45"/>
         <source>Path List</source>
         <translation>路径列表</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="75"/>
+        <location filename="xhash.ui" line="85"/>
         <source>Start</source>
         <translation>开始</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="97"/>
+        <location filename="xhash.ui" line="116"/>
         <source>Result</source>
         <translation>结果</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="212"/>
+        <location filename="xhash.ui" line="250"/>
         <source>File</source>
         <translation>文件</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="253"/>
+        <location filename="xhash.ui" line="329"/>
         <source>Total</source>
         <translation>总数</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="373"/>
+        <location filename="xhash.ui" line="488"/>
+        <source>Tools</source>
+        <translation>工具</translation>
+    </message>
+    <message>
+        <location filename="xhash.ui" line="494"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="383"/>
+        <location filename="xhash.ui" line="503"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="xhash.ui" line="368"/>
-        <source>Option</source>
-        <translation>选项</translation>
+        <location filename="xhash.ui" line="508"/>
+        <source>Config</source>
+        <translation>配置</translation>
     </message>
     <message>
         <location filename="xhash.cpp" line="130"/>
@@ -119,7 +157,7 @@
         <translation>不能打开文件</translation>
     </message>
     <message>
-        <location filename="xhash.cpp" line="220"/>
+        <location filename="xhash.cpp" line="219"/>
         <source>Calculate some file hash: </source>
         <translation>计算一些文件哈希: </translation>
     </message>
